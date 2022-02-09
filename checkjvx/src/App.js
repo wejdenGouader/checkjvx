b75543deb0a4241099dde546f6d8edac36efa062
wejdenGouader <wejjden.gdr@gmail.com>
2022-02-09 15:12:31 +0100

    tests

---
+++ checkjvx/src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+import src from './src.png';
+import video from './scr.mp4'
+
+function App() {
+  return (
+    <div className="App">
+      <div style={{border:'solid 1px black', maxWidth:'100vw'}}>
+
+<h1 className="title red">Your name here</h1>
+
+<br/>
+
+<img src={"/imageInSrc.jpg"} />
+
+<br >
+
+<img src={"/imageInPublic.jpg" }/>
+
+</div>
+
+<video width="320" height="240" controls>
+
+<source src='this is me.mp4' type='video/mp4' >
+
+</video>
+    </div>
+  );
+}
+
+export default App;
